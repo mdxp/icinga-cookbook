ab221f738fb610194b5d6aef5e0b30a1fc9ccf8d
Marius Ducea <marius.ducea@gmail.com>
2013-02-10 10:24:18 -0800

    bump version to 0.2.0 to prepare release on community site

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "marius@promethost.com"
 license          "Apache 2.0"
 description      "Installs/Configures Icinga"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          "0.2.0"
 
 %w{ apache2 build-essential }.each do |cb|
     depends cb
